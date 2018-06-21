31faa1fd8d1f95353a9e787c9e46c601d08eec5a
jojitoon <jojicartoon@gmail.com>
2018-06-21 10:08:45 +0100

    Write code snippet to create ride and validation

--- api/v1/controllers/ride.controller.js
+++ api/v1/controllers/ride.controller.js
@@ -1,5 +1,17 @@
 const { ridesDB } = require('../../../index.js');
+const joi = require('joi');
 
+
+const validator = (ride) => {
+  const schema = {
+    name: joi.string().min(3).required(),
+    from: joi.string().min(3).required(),
+    to: joi.string().min(3).required(),
+    driver: joi.string().min(3).required(),
+    time: joi.string().min(6).required(),
+  };
+  return joi.validate(ride, schema);
+};
 
 exports.all = (req, res) => {
   if (!ridesDB) {
@@ -9,3 +21,22 @@ exports.all = (req, res) => {
   }
   return res.send(ridesDB);
 };
+
+exports.create = (req, res) => {
+  const valid = validator(req.body);
+
+  if (valid.error) return res.status(400).send(valid.error.details[0].message);
+
+  const ride = {
+    id: ridesDB.length + 1,
+    name: req.body.name,
+    from: req.body.from,
+    to: req.body.to,
+    driver: req.body.driver,
+    time: req.body.time,
+  };
+  ridesDB.push(ride);
+
+  return res.status(201).send(ride);
+};
+
--- api/v1/routes/ride.route.js
+++ api/v1/routes/ride.route.js
@@ -3,4 +3,7 @@ const rides = require('../controllers/ride.controller.js');
 module.exports = (app) => {
 //   all ride
   app.get('/api/v1/rides', rides.all);
+
+   //   create new ride
+  app.post('/api/v1/rides', rides.create);
 };
