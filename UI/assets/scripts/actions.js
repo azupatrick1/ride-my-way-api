fb8070c0c4c1212f5a4cc7e12d14ac1426eb5eb7
jojitoon <jojicartoon@gmail.com>
2018-06-27 02:04:52 +0100

    Fix all navigation issues and pages

--- UI/assets/scripts/actions.js
+++ UI/assets/scripts/actions.js
@@ -1,3 +1,26 @@
 function openPage(page) {
    location.href= page;
 }
+
+function join() {
+
+
+    let  btn = document.getElementById('join');
+    let  btn2 = document.getElementById('join2');
+
+    if (btn) {
+        alert('Ride request has been sent');
+    btn.classList.remove("btn-green");
+    btn.classList.add("btn-red");
+    btn.innerText = "Cancel Request";
+    btn.id = "join2";
+    } else {
+    alert('Ride request has been cancelled');
+    btn2.classList.remove("btn-red");
+    btn2.classList.add("btn-green");
+    btn2.innerText = "Join this ride";
+    btn2.id = "join";
+    }
+    
+
+}
