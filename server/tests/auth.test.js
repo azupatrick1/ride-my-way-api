d3e65c9b5f819a884560fdca2a63c870bfde4fca
jojitoon <jojicartoon@gmail.com>
2018-07-02 07:12:17 +0100

    ft(#158751462/iniatial commit to cotinue
    inital commit to continue on anothor system

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -13,15 +13,35 @@ const userCredentials = {
   email: 'mocha@tester.com',
   password: 'chaiexpect',
 };
+const userCredentials2 = {
+  username: 'mochatester',
+  password: 'chaiexpect',
+};
+
 
 describe('testing signups', () => {
   it('should return token and success status', (done) => {
     request
-      .post('/auth/signup')
+      .post('/auth/signin')
+      .send(userCredentials2)
+      .expect(200)
+      .end((err, res) => {
+        expect(res.body.status).to.eql('success');
+        expect(res.body.data).have.property('token');
+      });
+    done();
+  });
+});
+
+describe('testing signins', () => {
+  it('should return token and success status', (done) => {
+    request
+      .post('/auth/signin')
       .send(userCredentials)
       .expect(200)
       .end((err, res) => {
         expect(res.body.status).to.eql('success');
+        expect(res.body.data).have.property('token');
       });
     done();
   });
