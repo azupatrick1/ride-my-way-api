55b8f2aa8f8c19e76a8e2637ed49de1326b2cc04
jojitoon <jojicartoon@gmail.com>
2018-07-15 13:48:26 +0100

    setup

--- UI/assets/scripts/fetch/signup.js
+++ UI/assets/scripts/fetch/signup.js
@@ -1,1 +1,31 @@
-const url = 'http://ride-my-way-api.herokuapp.com/api/v1/auth/signup';
+const signup = () => {
+  const url = "http://ride-my-way-api.herokuapp.com/";
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const passwordVerification = document.getElementById("password-verification")
+    .value;
+
+  //   if (password !== passwordVerification) {
+  //     console.log("password not match");
+  //   } else {
+  fetch(url
+    // , {
+////     //   method: "POST",
+//     mode: "cors"
+//     //   credentials: "include"
+//     //   headers: {
+//     //     "Content-Type": "application/json"
+//     //   },
+//     //   body: JSON.stringify({
+//     //     username: username,
+//     //     email: email,
+//     //     password: password
+//     //   })
+//   })
+)
+.then(response => response.json())
+    .then(result => console.log(result))
+    .catch(err => console.error(err));
+  //   }
+};
