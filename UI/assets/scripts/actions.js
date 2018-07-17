0ee7b33824fd448be9b3a1f2edf427458fa0d8c4
jojitoon <jojicartoon@gmail.com>
2018-07-17 16:17:04 +0100

    user can get ride and make requests to it
    [(finsihes): #159067571]

--- UI/assets/scripts/actions.js
+++ UI/assets/scripts/actions.js
@@ -2,16 +2,6 @@ function openPage(page) {
   location.href = page;
 }
 
-function join() {
-  let btn = document.getElementById("join");
-  let sign = document.getElementById("req-sent");
-
-  if (!classie.hasClass(btn, "disappear")) {
-    alert("Ride request has been sent");
-    classie.remove(sign, "disappear");
-    classie.add(btn, "disappear");
-  }
-}
 
 function notifyClose() {
   const notify = document.getElementById("notify");
