30545c575237f89b1e0e875a7b59256b89235c14
jojitoon <jojicartoon@gmail.com>
2018-07-27 05:25:07 +0100

    feat(ride) update rides
    User can update his ride if it has not been cancelled or completed by him

--- server/api/v1/controllers/Ride.js
+++ server/api/v1/controllers/Ride.js
@@ -21,7 +21,8 @@ class Ride {
 
   static createRide(req, res) {
     const data = [req.body.name, req.body.location, req.body.destination,
-      Number(req.body.slot), req.body.time, req.currentUser.id, req.currentUser.username, req.body.carModel, req.body.takeOffDate];
+      Number(req.body.slot), req.body.time, req.currentUser.id,
+      req.currentUser.username, req.body.carModel, req.body.takeOffDate];
     if (req.checkRide) {
       res.jsend.fail({ message: 'you are denied: you still have a pending ride' });
     } else {
@@ -46,8 +47,46 @@ class Ride {
     res.jsend.success({ ride });
   }
 
+  static editRide(req, res) {
+    const data = [req.body.name, req.body.location, req.body.destination,
+      Number(req.body.slot), req.body.time, req.body.carModel, req.body.takeOffDate, req.ride.id];
+
+    const sql = 'UPDATE rides SET name = $1, location = $2, destination = $3, slot= $4, time = $5, carModel = $6, takeOffDate = $7, updated_at = NOW() WHERE id = $8 RETURNING *';
+    if (req.currentUser.id !== req.ride.user_id) {
+      res.status(403).jsend.fail({ message: 'you can not update someone else ride' });
+    } else if (req.ride.status === 'cancelled') {
+      res.status(400).jsend.fail({ message: 'ride is already cancelled' });
+    } else if (req.ride.status === 'completed') {
+      res.status(400).jsend.fail({ message: 'ride is already completed' });
+    } else {
+      pool((err, client, done) => {
+        if (err) res.status(500).jsend.error({ message: 'error connecting to database' });
+
+        return client.query(sql, data, (error, ride) => {
+          done();
+          if (error) res.status(400).jsend.error({ message: 'error updating ride' });
+
+          res.jsend.success({ ride: ride.rows[0] });
+        });
+      });
+    }
+  }
   static deleteRide(req, res) {
     const sql = 'UPDATE rides SET status = $1 WHERE id = $2';
+
+    const alterRequest = (status) => {
+      const sql2 = 'UPDATE requests SET status = $1 WHERE ride_id = $2';
+
+      pool((err, client, done) => {
+        if (err) res.status(500).jsend.error({ message: 'error connecting to database' });
+
+        client.query(sql2, [status, req.ride.id], (error) => {
+          done();
+          if (error) res.status(500).jsend.error({ message: 'could not access request' });
+        });
+      });
+    };
+
     if (req.currentUser.id !== req.ride.user_id) {
       res.status(403).jsend.fail({ message: 'you can not cancel someone else ride' });
     } else if (req.ride.status === 'cancelled') {
@@ -59,7 +98,7 @@ class Ride {
         client.query(sql, ['cancelled', req.params.rideId], (error) => {
           done();
           if (error) res.status(400).jsend.error({ message: 'error while trying to cancel ride' });
-
+          alterRequest('Ride has been cancelled');
           res.jsend.success({ message: 'The ride has been cancelled' });
         });
       });
